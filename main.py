bbf01ae3a9d439764851a35e132e8e3bd1e088d7
Govinda-Upadhya <143345467+Govinda-Upadhya@users.noreply.github.com>
2023-10-21 12:45:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,23 @@ sc.onkey(l_paddle.go_down, "s")
 
 game_is_on=True
 
-while game_is_on:
+while True:
     sc.update()
+    ball.move()
+
+    if ball.ycor()>280 or ball.ycor()< -280 :
+        ball.bouncey()
+    if ball.distance(pad1)<55 and ball.xcor()>340 or ball.distance(pad2)<55 and ball.xcor()>-340:
+        ball.bouncex()
+    if ball.xcor()>500 :
+        ball.miss()
+        scoreborad.rscore()
+        ball.bouncex()
+    if ball.xcor() < -500:
+        ball.miss()
+        scoreborad.lscore()
+        ball.bouncex()
+
 
 
 sc.exitonclick()
